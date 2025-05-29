2e91f72f29490591c80e8b97a581c63a64133ab0
Ross-H-U <ross.cantor@healthuniverse.com>
2025-05-29 11:40:53 -0400

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from router import router
+from api.router import router
 
 app = FastAPI(
     title="DCRI eConsent Dashboard Demo FastAPI",
